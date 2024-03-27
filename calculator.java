d7cdd8efcb5ce0b2e5a4d1a4c77073e0bc9ecdfe
Ori <150516651+coxxlongg@users.noreply.github.com>
2024-03-27 23:15:03 +0500

    no public

--- calculator.java
+++ calculator.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator implements ActionListener{
+class Calculator implements ActionListener{
 
     JFrame frame;
     JTextField textfield;
